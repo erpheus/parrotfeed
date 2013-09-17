dae6abfba932ecde8e7d48259c73302ae65bfe0d
Erpheus <erpheus@gmail.com>
2013-09-17 14:29:21 +0200

    initial rails commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Abroadguille::Application.config.secret_token = 'b6866e411b526c3e1c7f665ecbd63ed009f1027b46b852c1476d2e159a2832635876e709f393d7681cdcd45f7037c063c694034cb9cccb8de978d718da141e00'
