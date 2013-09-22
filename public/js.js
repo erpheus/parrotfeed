b9cea03ed389ec2be4e5494a6d144484f2db0401
Erpheus <erpheus@gmail.com>
2013-09-22 23:43:22 +0200

    Temporal web interface. Now connected to the API

---
+++ public/js.js
@@ -0,0 +1,25 @@
+$(function(){
+	var data = $.getJSON("/api/stories.json")
+	var container = $("#content");
+	data.then(function(result){
+		for (var i = 0; i < result.length; i++) {
+			var story = result[i];
+			var element = $("<div />",{class: "story"});
+			for (var j = 0; j < story.texts.length; j++) {
+				var text = $("<div />",{class: "text"});
+				if (story.texts[j].pictures !== undefined){
+					for (var k = 0; k < story.texts[j].pictures.length; k++) {
+						text.append($("<img />",{
+							src: story.texts[j].pictures[k].preview_url,
+							class: "pull-right"
+						}));
+					};
+				}
+
+				text.append("<p>"+story.texts[j].content+"</p>");
+				element.append(text);
+			};
+			container.append(element);
+		};
+	});
+});
